5fcea05a04b0b76329cb48aa1d0b63b1a41fdff7
The-Barer <dmitry.barer@icloud.com>
2024-02-21 11:17:29 +0300

    refactor start

---
+++ src/Shared/Config/config.ts
@@ -0,0 +1,17 @@
+const APP_DEV_MODE: boolean = import.meta.env.VITE_APP_DEV_MODE === 'true'
+
+const APP_HOST = APP_DEV_MODE ? import.meta.env.VITE_APP_DEV_HOST : import.meta.env.VITE_APP_HOST
+
+const API_ENDPOINT = APP_DEV_MODE
+    ? import.meta.env.VITE_API_DEV_ENDPOINT
+    : import.meta.env.VITE_API_ENDPOINT
+
+const AUTH_REDIRECT_URL = `${APP_HOST}/auth/callback`
+
+console.log(API_ENDPOINT, APP_DEV_MODE)
+
+export const config = {
+    APP_HOST,
+    API_ENDPOINT,
+    AUTH_REDIRECT_URL,
+} as const
